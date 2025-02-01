4eb6057ef127ce93f2c8b41a5cf26160bc35bb29
Jason Overmier <jason.r.overmier@gmail.com>
2025-02-01 15:30:28 -0600

    feat: improve sortable props for better flexibility with react-hook-form; next 15

--- next.config.js
+++ next.config.js
@@ -2,13 +2,13 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.js");
+await import("./src/env.js")
 
 /** @type {import("next").NextConfig} */
 const config = {
   experimental: {
-    ppr: true,
+    reactCompiler: true,
   },
-};
+}
 
-export default config;
+export default config
